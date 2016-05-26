861a9c56206d0181025c62594890e1fbe5fee248
Mahfuz2811 <mahfuzshishir2811@gmail.com>
2016-05-27 00:16:59 +0600

    controller created
    - controller created

--- app/Http/Controllers/MakerController.php
+++ app/Http/Controllers/MakerController.php
@@ -6,6 +6,7 @@ use Illuminate\Http\Request;
 
 use App\Http\Requests;
 use App\Models\Maker;
+use App\Models\Vehicle;
 use App\Http\Requests\CreateMakerRequest;
 
 class MakerController extends Controller
@@ -23,6 +24,7 @@ class MakerController extends Controller
     	{
     		return response()->json(['message'=>'This maker does not exit','code'=>404],404);
     	}
+
     	return response()->json(['data'=> $makers],200);
     }
 
@@ -31,5 +33,41 @@ class MakerController extends Controller
     	$values = $request->only(['name','phone']);
     	Maker::create($values);
     	return response()->json(['message' => 'maker correctly added'], 201);
+    }
+
+    public function update(CreateMakerRequest $request, $id)
+    {
+        $maker = Maker::find($id);
+        if(!$maker)
+        {
+            return response()->json(['message'=>'This maker does not exit','code'=>404],404);
+        }
+
+        $name = $request['name'];
+        $phone = $request['phone'];
+
+        $maker->name = $name;
+        $maker->phone = $phone;
+
+        $maker->save();
+        return response()->json(['message' => 'maker correctly updated'], 201);
+    }
+
+    public function destroy($id)
+    {
+        $maker = Maker::find($id);
+        if(!$maker)
+        {
+            return response()->json(['message'=>'This maker does not exit','code'=>404],404);
+        }
+
+        $vehicles = $maker->vehicles;
+        if(sizeof($vehicles)>0)
+        {
+            return response()->json(['message'=>'This maker has associated with vehicles. Delete his vehicles first','code'=>409],409);
+        }
+
+        $maker->delete();
+        return response()->json(['message' => 'THe maker has been deleted successfully'], 201);
     }
 }
--- app/Http/Controllers/MakerVehicleController.php
+++ app/Http/Controllers/MakerVehicleController.php
@@ -36,4 +36,49 @@ class MakerVehicleController extends Controller
     	$maker->vehicles()->create($values);
     	return response()->json(['message' => 'The vehicle associated was created']);
     }
+
+    public function show($id, $vehicle_id)
+    {
+        $maker = Maker::find($id);
+        if(!$maker)
+        {
+            return response()->json(['message' => 'This maker does not exit', 'code'=> 404], 404);
+        }
+
+        $vehicle = $maker->vehicles->find($vehicle_id);
+        if(!$vehicle)
+        {
+            return response()->json(['message' => 'This vehicle does not spacify', 'code' => 404], 404);
+        }
+
+        return response()->json(['data' => $vehicle], 200);
+    }
+
+    public function update(CreateVehicleRequest $request, $maker_id, $vehicle_id)
+    {
+        $maker = Maker::find($maker_id);
+        if(!$maker_id)
+        {
+            return response()->json(['message' => 'This maker does not spacify', 'code' => 404], 404);
+        }
+
+        $vehicle = $maker->vehicles->find($vehicle_id);
+        if(!$vehicle)
+        {
+            return response()->json(['message' => 'This vehicle does not spacify', 'code' => 404], 404);
+        }
+
+        $color = $request->get('color');
+        $power = $request->get('power');
+        $capacity = $request->get('capacity');
+        $speed = $request->get('speed');
+
+        $vehicle->color = $color;
+        $vehicle->power = $power;
+        $vehicle->capacity = $capacity;
+        $vehicle->speed = $speed;
+
+        $vehicle->save();
+        return response()->json(['message' => 'vehicle correctly updated'], 201);
+    }
 }
